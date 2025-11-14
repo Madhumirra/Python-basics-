893b88593b53035a1b5fd1a50d257a8255e823c5
MADHUMIRRA R <madhumirra696@gmail.com>
2025-11-14 12:00:49 +0530

    list.py

---
+++ list.py
@@ -0,0 +1,23 @@
+#List Class
+#All the list that are created  are the objects of the list class in Python.
+
+nums=[1,2,3,4]
+print(type(nums))
+
+mylist=list('Hello') #Here list is a constructor
+print(mylist)
+#output
+#class 'list'>
+#['H', 'e', 'l', 'l', 'o']
+
+#Iterate List
+#A list items can be iterate using the for loop.
+
+names=["Jeff", "Bill", "Steve", "Mohan"]
+
+for name in names:
+    print(name)
+#Jeff
+#Bill
+#Steve
+#Mohan
